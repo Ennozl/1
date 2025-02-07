a9e8450cff5a56bff1a40cfb78716bde4478ce1d
Ennozl <147396238+Ennozl@users.noreply.github.com>
2025-02-07 14:42:33 +0800

    Add files via upload

---
+++ Model/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Model
+{
+    
+    public partial class Admin
+    {
+        public Admin()
+        {
+
+        }
+        public int admin_id { get; set; }
+        public string admin_name { get; set; }
+        public string pwd { get; set; }
+    }
+}
+
---
+++ UI/login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Model.Admin m_admin = new Model.Admin();
+            m_admin.admin_name = textBox1.Text;
+            m_admin.pwd = textBox2.Text;
+
+            BLL.B_Admin b_Admin = new BLL.B_Admin();
+            if (b_Admin.Login(m_admin) == 0)
+            {
+                
+                this.DialogResult = DialogResult.OK;
+                main main=new main();
+                main.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("登录失败");
+                textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+            }
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
